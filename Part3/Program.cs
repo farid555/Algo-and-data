0d7724967b5fd64ef10ea544ae2f8a9f7c1f64f4
farid555 <mohammad.uzzaman2@centria.fi>
2020-05-26 18:14:02 +0300

    it's done

--- Part3/Program.cs
+++ Part3/Program.cs
@@ -1,14 +1,54 @@
 using System;
-namespace part3 {
-    class Program {
-        public static void Main (string[] args) {
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
 
+
+namespace part3 
+{
+    
+        
+  class Program
+  {
+    public static int[] Randomizer(int n)
+    {
+      Random random = new Random();
+      int[] arr = new int[n];
+      for (int i = 0; i < arr.Length; i++)
+      {
+        // integers between 1 and 1000 are enough for us
+        arr[i] = random.Next(1, 1001);
+      }
+      return arr;
+    }
+
+     static void Main(string[] args){
+      
+        SmallestDifference s = new SmallestDifference();
+       Console.WriteLine(s.Calculate(new int[] { 4, 1, 8, 5 })); // 1
+       Console.WriteLine(s.Calculate(new int[] { 1, 10, 100 })); // 9
+       Console.WriteLine(s.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 0
+       Console.WriteLine(s.Calculate(Randomizer(11))); // depends on random
+
+      
+
+      
+
+    }
+  }
+  
+}
+
+        
+             //exercise1
             //PrintHello printHello = new PrintHello();
             //printHello.Hello(8);
             
             //Console.WriteLine();
 
 
+            /*
+            exercise4
             BinarySearch b = new BinarySearch ();
             Console.WriteLine (b.Find ((new int[] { 4, 1, 8, 5 }), 2)); // false
             Console.WriteLine (b.Find ((new int[] { 0, 0 }), 0)); // true
@@ -16,16 +56,5 @@ namespace part3 {
             Console.WriteLine (b.Find ((new int[] { 0 }), 0)); // true
             Console.WriteLine();
             Console.WriteLine (b.Find ((Randomizer (1000)), 3)); // ?
-        }
-        public static int[] Randomizer (int n) {
-            Random random = new Random ();
-            int[] arr = new int[n];
-            for (int i = 0; i < arr.Length; i++) {
-                // integers between 1 and 1000 are enough for us
-                arr[i] = random.Next (1, 1001);
-            }
-            return arr; 
-        }
-    }
-
-}
+        }*/
+     
---
+++ Part3/exercise2.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Text;
+
+namespace part3
+{
+     public class SmallestDifference
+     {
+         public int Calculate(int[] t)
+         {
+             Array.Sort(t);
+
+             int difference = int.MaxValue;{
+
+             for (int  i = 0; i < t.Length - 1; i++)
+             {
+                 if (((t[i+1] - t[i]) < difference))
+                 {
+                     difference = t[i+1] - t[i];
+                 }
+             }
+
+         }
+         return difference;
+        }
+     }
+}
